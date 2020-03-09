ed42c21ccf47e11dfdd99d3061af1077b25a973d
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-03-09 16:43:11 -0700

    test

---
+++ ToDoApp/ToDoApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp
+{
+    public partial class App : Application
+
+    {
+        public static string FolderPath { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            FolderPath = Environment.GetFolderPath(
+                  Environment.SpecialFolder.LocalApplicationData);
+
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
